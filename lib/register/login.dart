72b0195e3cbe5a95182757f8e87393fdcffdfb84
ShreyasRaj4 <srigadha.shreyasraj.cse19@itbhu.ac.in>
2020-10-28 08:20:23 +0530

    Login Page

--- lib/register/login.dart
+++ lib/register/login.dart
@@ -1,7 +1,10 @@
 import 'package:MediCaP/authentication_service.dart';
-import 'package:MediCaP/register/resistration.dart';
+import 'package:MediCaP/register/signup.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class Login extends StatefulWidget {
@@ -12,132 +15,180 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   bool _showpassword = true;
 
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
+  final _formkey = GlobalKey<FormState>();
+
+  String _email, _password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      resizeToAvoidBottomInset: false,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 60,
-              width: 100,
-              margin: EdgeInsets.only(top: 50),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                'MediCaP',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 20),
-              child: Text(
-                'Account login',
-                style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 30,
-              ),
-              child: TextField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.mail_outline_outlined),
-                  labelText: 'Email ID',
-                ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 30,
-              ),
-              child: TextField(
-                controller: passwordController,
-                obscureText: _showpassword,
-                decoration: InputDecoration(
-                  labelText: 'password',
-                  prefixIcon: Icon(Icons.security),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      Icons.remove_red_eye,
-                      color: this._showpassword ? Colors.indigo : Colors.orange,
+      backgroundColor: Colors.blue[200],
+      body: Container(
+        child: Form(
+          autovalidateMode: AutovalidateMode.always,
+          key: _formkey,
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: 50,
+                    left: 120,
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      child: FlareActor(
+                        'images/CBvirus',
+                        animation: 'move',
+                        fit: BoxFit.fill,
+                      ),
                     ),
-                    onPressed: () {
-                      setState(() => this._showpassword = !this._showpassword);
-                    },
                   ),
-                ),
-              ),
-            ),
-            Container(
-              width: double.infinity,
-              margin: EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 30,
-              ),
-              child: RaisedButton(
-                onPressed: () {
-                  context.read<AuthenticationService>().signIn(
-                        email: emailController.text.trim(),
-                        password: passwordController.text.trim(),
-                      );
-                },
-                child: Text(
-                  'log in',
-                  style: TextStyle(color: Colors.white),
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                color: Colors.redAccent,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 30,
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: Text('forget password'),
+                  Positioned(
+                    top: 200,
+                    left: 20,
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        fontSize: 50,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                  Expanded(
-                    flex: 0,
-                    child: RaisedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Signup(),
+                  Positioned(
+                    top: 270,
+                    left: 20,
+                    child: Text(
+                      'Please login to continue.',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontStyle: GoogleFonts.mavenPro().fontStyle,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 310,
+                    left: 10,
+                    child: Container(
+                      height: 290,
+                      width: 370,
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top: 15.0),
+                            child: TextFormField(
+                              controller: _emailController,
+                              onSaved: (input) => _email = input,
+                              validator: MultiValidator([
+                                RequiredValidator(errorText: 'Required'),
+                                EmailValidator(errorText: 'Not a valid Email')
+                              ]),
+                              decoration: InputDecoration(
+                                  prefixIcon: Icon(Icons.email),
+                                  hintText: "Email"),
+                            ),
                           ),
-                        );
+                          Padding(
+                            padding: EdgeInsets.only(top: 25.0),
+                            child: TextFormField(
+                              controller: _passwordController,
+                              onSaved: (input) => _password = input,
+                              validator: MultiValidator([
+                                RequiredValidator(errorText: 'Required'),
+                                MinLengthValidator(6,
+                                    errorText:
+                                        'Should be atleast 6 charecters'),
+                                MaxLengthValidator(15,
+                                    errorText: 'Should be 15 charecters Max')
+                              ]),
+                              obscureText: _showpassword,
+                              decoration: InputDecoration(
+                                hintText: "Password",
+                                prefixIcon: Icon(Icons.lock),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    Icons.remove_red_eye,
+                                    color: this._showpassword
+                                        ? Colors.indigo
+                                        : Colors.orange,
+                                  ),
+                                  onPressed: () {
+                                    setState(() => this._showpassword =
+                                        !this._showpassword);
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment(1, 6),
+                            child: MaterialButton(
+                              onPressed: () {},
+                              minWidth: 180,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20.0))),
+                              padding: EdgeInsets.symmetric(vertical: 10.0),
+                              child: Text(
+                                'forgot password?',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black),
+                              ),
+                            ),
+                          ),
+                          MaterialButton(
+                            onPressed: () {
+                              context.read<AuthenticationService>().signIn(
+                                    email: _emailController.text.trim(),
+                                    password:
+                                        _passwordController.text.trim(),
+                                  );
+                            },
+                            minWidth: 220,
+                            splashColor: Colors.red[500],
+                            color: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.all(
+                                    Radius.circular(20.0))),
+                            padding: EdgeInsets.symmetric(vertical: 15.0),
+                            child: Text(
+                              'login',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 660,
+                    left: 95,
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => Signup()));
                       },
-                      child: Text('Register Account'),
+                      child: Text(
+                        'Don\'t have an account?',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
                     ),
                   ),
                 ],
-              ),
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
